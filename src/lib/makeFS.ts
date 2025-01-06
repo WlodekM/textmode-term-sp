69a22c54fe26ca80302865e0ae465404ffa97355
WlodekM <wlodzio.maly@gmail.com>
2025-01-06 14:18:40 +0200

    initial fs with modes

--- src/lib/makeFS.ts
+++ src/lib/makeFS.ts
@@ -2,18 +2,20 @@ import System from "../term/main.ts";
 import commands from "./commands.ts";
 
 export default async function makeFS(this: System) {
-    this.fs.mkdirSync('/home')
-    this.fs.mkdirSync('/etc')
-    this.fs.mkdirSync('/usr')
-    this.fs.mkdirSync('/usr/bin')
+    const mode = (m: number) => parseInt(m.toString(), 8)
+    this.fs.mkdirSync('/home', { mode: mode(755) })
+    this.fs.mkdirSync('/etc', { mode: mode(755) })
+    this.fs.mkdirSync('/usr', { mode: mode(755) })
+    this.fs.mkdirSync('/usr/bin', { mode: mode(755) })
     this.fs.symlinkSync('/usr/bin', '/bin')
     for (const [uid, user] of this.users.entries()) {
-        this.fs.mkdirSync(user.home)
+        this.fs.mkdirSync(user.home, { mode: 750 })
         this.fs.chownSync(user.home, uid, user.gid)
     }
     for (const cmd of commands) {
         const command = (await import(`../commands/${cmd}.js`)).default;
         this.fs.writeFileSync(`/usr/bin/${cmd}.js`, 'export default ' + command.toString())
+        this.fs.chownSync(`/usr/bin/${cmd}.js`, 0, 0)
         this.fs.chmodSync(`/usr/bin/${cmd}.js`, parseInt('775', 8))
     }
 }
