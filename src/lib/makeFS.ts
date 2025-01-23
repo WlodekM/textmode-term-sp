148957a4555087709f61a026707f9855ddc1fde7
WlodekM <wlodzio.maly@gmail.com>
2025-01-23 10:42:36 +0200

    finally fixed perms

--- src/lib/makeFS.ts
+++ src/lib/makeFS.ts
@@ -9,13 +9,13 @@ export default async function makeFS(this: System) {
     this.fs.mkdirSync('/usr/bin', { mode: mode(755) })
     this.fs.symlinkSync('/usr/bin', '/bin')
     for (const [uid, user] of this.users.entries()) {
-        this.fs.mkdirSync(user.home, { mode: 750 })
+        this.fs.mkdirSync(user.home, { mode: mode(750) })
         this.fs.chownSync(user.home, uid, user.gid)
     }
     for (const cmd of commands) {
         const command = (await import(`../commands/${cmd}.js`)).default;
         this.fs.writeFileSync(`/usr/bin/${cmd}.js`, 'export default ' + command.toString())
         this.fs.chownSync(`/usr/bin/${cmd}.js`, 0, 0)
-        this.fs.chmodSync(`/usr/bin/${cmd}.js`, parseInt('775', 8))
+        this.fs.chmodSync(`/usr/bin/${cmd}.js`, mode(777))
     }
 }
